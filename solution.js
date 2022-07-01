dfbd18604a58212edb58ab6b2556fe16f6318e23
xstartstaticx <daniel.reiller@icloud.com>
2022-07-01 11:12:17 +0200

    finished exercise

---
+++ solution.js
@@ -0,0 +1,37 @@
+// 0
+let snackServings = 350;
+let guests = 200;
+// 1
+console.log(`${snackServings + guests}`);
+// 2
+console.log(`${snackServings - guests}`);
+console.log(`${guests - snackServings}`);
+// 3
+console.log(`${snackServings * guests}`);
+// 4
+console.log(`${snackServings / guests}`);
+// 5
+let drinks = 10;
+let resultOne = (snackServings * guests) / drinks;
+console.log(`${resultOne}`);
+// 6
+let monkeys = 15;
+let bananas = 9;
+console.log(`${monkeys / bananas}`);
+// 7
+let monkeyFights = 20;
+let resultTwo = (monkeys + bananas) * monkeyFights;
+console.log(`${resultTwo}`);
+// 8
+let monkeyBusiness = 90;
+monkeyBusiness++;
+console.log(monkeyBusiness);
+// 9
+let boredom = 13;
+boredom--;
+console.log(boredom);
+// 10
+let happiness = monkeys - bananas;
+console.log(`${happiness + monkeyFights}`);
+// 11
+console.log(`${resultOne % resultTwo}`);
